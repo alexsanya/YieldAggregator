4f0a81c7416b22d010a3ac15a4e404297a0b2091
alexsanya <alexsanyakoval@gmail.com>
2023-06-06 23:10:30 -0500

    modified deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,14 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  const Aggregator = await ethers.getContractFactory("Aggregator");
+  const aggregator = await Aggregator.deploy();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  await aggregator.deployed();
 
   console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `Aggregator deployed to ${aggregator.address}`
   );
 }
 
